f21563c30bea451a1343b16ad8a3c2186618c92b
ericjung04 <ericjung1705@gmail.com>
2025-04-09 11:09:08 -0400

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Hello, my name is Eric Jung</h1>
+        <p>Welcome to my portfolio!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
